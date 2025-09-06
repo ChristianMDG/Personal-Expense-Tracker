303f87a4acf6e336d6bc103efe02f1521cff0a66
ChristianMDG <christian.ravelojaona@passerellesnumeriques.org>
2025-09-06 20:53:05 +0300

    config server.js pour interagir avec le frontend

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+const incomesRoute = require('./routes/incomes')
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors({
+  origin: "process.env.FRONTEND_URL ||'http:localhost:3000'",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-// API routes
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
-});
-
-// Catch-all handler to serve the React app for any other requests
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
-
+app.use('/api/incomes',incomesRoute)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-
 });
