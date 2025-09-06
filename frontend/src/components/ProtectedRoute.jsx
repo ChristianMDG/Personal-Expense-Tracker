41be04834d1ab184be2be8a600cf2f38dead7fa9
ChristianMDG <christian.ravelojaona@passerellesnumeriques.org>
2025-09-06 22:55:28 +0300

    test frontend Income

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { useAuth } from '../contexts/AuthContext';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return user ? children : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
